8c5ca4d4d9a945e830ec5e5f546d9f68122d7b39
Glitch (grimmxiii) <none>
2022-09-21 19:48:12 +0000

    😲📟 Updated with Glitch x6

--- src/pages/custo.jsx
+++ src/pages/custo.jsx
@@ -9,19 +9,40 @@ import account from "../../database.json";
 
 
 export default function Home() {
+  useEffect(() => {
 
+  },[])
+  const [cookies, setCookie, removeCookie] = useCookies(["user"])
+  const [colorH, setColorH] = useState()
+  const [colorHText, setColorHText] = useState()
+  
+  function colors() {
+    if (cookies.colorH) {
+      setColorH('' + cookies.colorH + '')
+    }
+  }
+  
+  function setCookieH() {
+    setCookie('colorH', colorHText, { path: '/'})
+    window.location.replace('/')
+
+  }
+
+  
+  
   
   return (
     <>
       <table>
         <tr>
           <th>
-
-          
+            <input id='amongus' onChange={(e) => setColorHText(e.target.value)}/>
+            <button onClick={setCookieH}>This is a button</button>
+            {colorHText}
           </th>
           <th>
-            <div className="blogTop">adf</div>
-            <div className="blogBot">adf</div>
+            <h1 style={{color:colorH}}>This is a title</h1>
+
             
             <div className="blogTop">adf</div>
             <div className="blogBot">adf</div>
